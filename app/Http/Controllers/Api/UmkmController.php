0bae80cf9d70aa72b22cc9d8e1709324a8f29a7c
woyo <adisiswoyo@gmail.com>
2021-10-08 16:24:54 +0700

    Add Swagger Documentation

--- app/Http/Controllers/Api/UmkmController.php
+++ app/Http/Controllers/Api/UmkmController.php
@@ -26,6 +26,55 @@ class UmkmController extends Controller
         return (round($distance, 2));
     }
 
+    /**
+     * @OA\Post(
+     ** path="/v1/user-login",
+     *   tags={"Login"},
+     *   summary="Login",
+     *   operationId="login",
+     *
+     *   @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *          type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *)
+     **/
+
     public function umkm()
     {
         $data = UmkmModel::get();
---
+++ app/Http/Swagger/swagger_models/settings/sample_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class SampleController //This is a sample laravel Controller
+
+{
+
+
+
+    public function sampleFunctionWithDoc()
+
+    {
+    }
+}
